3aeee80dbc206aa7f88c72e6bcdea7685028ce8b
Arylen <177881981+Arylen@users.noreply.github.com>
2026-02-11 13:36:30 -0500

    Refactor MOVs

--- ASpace/Core/Emulation/Components/OpTable.cs
+++ ASpace/Core/Emulation/Components/OpTable.cs
@@ -32,8 +32,6 @@ public static class OpTable
         {
             var registers = Registers.GetEncodedRegs(op);
             var dest = registers.Destination;
-            if (registers.Source != Registers.Name.M)
-                throw new ArgumentException($"MVI op source register was NOT memory? {op:X2} - {registers.Source}");
             _ops[op] = new CpuOp(
                 $"MVI {dest}, %d8",
                 (cpu) =>
@@ -60,89 +58,35 @@ public static class OpTable
             0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4F,
             0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5F,
             0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6F,
-            0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7F,
-            
+                                                      0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7F,
             // 7-cycle Reg-to-Mem MOVs
             0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x77,
-
             // 7-cycle Mem-to-Reg MOVs
             0x46, 0x56, 0x66, 0x4E, 0x5E, 0x6E, 0x7E, 
         })
         {
-            
-        }
-        
-        // Register-to-Register MOV ops, 5 cycles
-        {
-            byte[] movOps = 
-            [
-                0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4F,
-                0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5F,
-                0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6F,
-                0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7F,
-            ];
-            foreach (var op in movOps)
-            {
-                var opRegisters = Registers.GetEncodedRegs(op);
-                _ops[op] = new CpuOp(
-                    $"MOV {opRegisters.Destination}, {opRegisters.Source}",
-                    (cpu) =>
+            var registers = Registers.GetEncodedRegs(op);
+            var dest = registers.Destination;
+            var src = registers.Source;
+            _ops[op] = new CpuOp(
+                $"MOV {dest}, {src}",
+                (cpu) =>
+                {
+                    // Reg-to-reg MOVs
+                    if (dest != Registers.Name.M && src != Registers.Name.M)
                     {
-                        var sourceValue = cpu.Reg.GetByName(opRegisters.Source);
-                        cpu.Reg.SetByName(opRegisters.Destination, sourceValue);
+                        cpu.Reg.SetByName(dest, cpu.Reg.GetByName(src));
                         return 5;
                     }
-                );
-            }
-        }
-
-        // Write-To-Memory MOV Ops
-        {
-            byte[] moveToMemOps =
-            [
-                0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x77
-            ];
-            foreach (var op in moveToMemOps)
-            {
-                var registers = Registers.GetEncodedRegs(op);
-                if (registers.Destination != Registers.Name.M)
-                    throw new Exception($"Expected destination of MemoryAddress (HL/M) when making op table. Got {registers.Destination} for opcode {op:X2}");
-                _ops[op] = new CpuOp(
-                    $"MOV {registers.Destination}, {registers.Source}",
-                    (cpu) =>
-                    {
-                        // Place source register value into memory address (HL)
-                        var address = cpu.Reg.HL;
-                        cpu.Bus.WriteByte(address, cpu.Reg.GetByName(registers.Source));
-                        return 7;
-                    }
-                );
-            }
-        }
-        
-        // Register-from-memory MOV Ops
-        {
-            byte[] moveFromMemOps =
-            [
-                0x46, 0x56, 0x66,
-                0x4E, 0x5E, 0x6E, 0x7E, 
-            ];
-
-            foreach (var op in moveFromMemOps)
-            {
-                var registers = Registers.GetEncodedRegs(op);
-                if (registers.Source != Registers.Name.M)
-                    throw new Exception($"Expected source of MemoryAddress (HL/M) when making op table. Got {registers.Source} for opcode {op:X2}");
-                _ops[op] = new CpuOp(
-                    $"MOV {registers.Destination}, {registers.Source}",
-                    (cpu) =>
-                    {
-                        var memValue = cpu.Bus.ReadByte(cpu.Reg.HL);
-                        cpu.Reg.SetByName(registers.Destination, memValue);
-                        return 7;
-                    }
-                );
-            }
+                    
+                    if (src == Registers.Name.M)
+                        cpu.Reg.SetByName(dest, cpu.Bus.ReadByte(cpu.Reg.HL));
+                    else
+                        cpu.Bus.WriteByte(cpu.Reg.HL, cpu.Reg.GetByName(src));
+                    
+                    return 7;
+                }
+            );
         }
         
         // HLT
