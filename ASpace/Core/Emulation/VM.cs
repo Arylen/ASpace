415e319c56d9bd331433f91f74fe326d2c69a090
Arylen <177881981+Arylen@users.noreply.github.com>
2026-02-12 08:38:49 -0500

    Add CycleCount to VM

--- ASpace/Core/Emulation/VM.cs
+++ ASpace/Core/Emulation/VM.cs
@@ -6,10 +6,25 @@ public class VM
 {
     public CPU CPU { get; set; }
     public Bus Bus { get; set; }
+
+    public ulong CycleCount;
     
     public VM()
     {
         Bus = new Bus();
         CPU = new CPU(Bus);
+    }
+
+    public void LoadRom(byte[] data, ushort addressStart = 0)
+    {
+        for (var i = 0; i < data.Length; i++)
+            Bus.WriteByte((ushort)(addressStart + i), data[i]);
+    }
+
+    public void Step()
+    {
+        var cycles = CPU.ExecuteNext();
+        
+        CycleCount += (ulong)cycles;
     }
 }
